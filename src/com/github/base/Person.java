bbe899cb8241aaab7abaa7ca8e97dd616b6b2c13
fateme <fatemesadatsedigh@gmail.com>
2025-03-01 14:52:42 +0330

    build person class

---
+++ src/com/github/base/Person.java
@@ -0,0 +1,26 @@
+package com.github.base;
+
+import java.util.ArrayList;
+
+public class Person{
+    public int id;
+    public static ArrayList<Person> personList = new ArrayList<>();
+    public String name;
+    public final String nationalID;
+
+    public Person(String name, String nationalID){
+        this.name = name;
+        this.nationalID = nationalID;
+        personList.add(this);
+        id = personList.size();
+    }
+
+    public static Person findByID(int id){
+        for(Person findPerson : personList){
+            if(findPerson.id == id){
+                return findPerson;
+            }
+        }
+        return null;
+    }
+}
